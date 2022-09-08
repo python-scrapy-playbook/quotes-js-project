5f5a235e96dedd3f469dd85145a4f0d47b621931
Joe Kearney <josephkearney91@gmail.com>
2022-09-08 16:10:56 +0200

    initial commit

---
+++ quotes_js_scraper/spiders/quotes.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        pass
